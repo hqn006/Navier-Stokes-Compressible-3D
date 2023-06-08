7543baa8b866d2a8824a31a585e21886603f426a
hqn006 <hqn006@ucsd.edu>
2023-06-07 22:15:41 -0700

    Revert "Update ddy_fwd.m"
    This reverts commit 3ec2274ef67c36993feb7ad3621bf42d4b66f72f.

--- MATLAB/functions/ddy_fwd.m
+++ MATLAB/functions/ddy_fwd.m
@@ -29,7 +29,7 @@ for i=1:size(f,1)
             df(i,j,k)=(f(i,j+1,k)-f(i,j,k))/dy;
         end
         %Normal
-    % BC    df(i,end,k)=(f(i,end,k)-f(i,end-1,k));
+        df(i,end,k)=(f(i,end,k)-f(i,end-1,k));
     %Periodic
        % df(i,end)=(f(i,1)-f(i,end))/dy;
     end
