2de7c870a2cb89d9ecbb0c49408220b36016044b
hqn006 <hqn006@ucsd.edu>
2023-06-07 22:15:52 -0700

    Revert "Update ddz_central.m"
    This reverts commit 599c68df967cdcab8ba742eb253e651a2de6cf40.

--- MATLAB/functions/ddz_central.m
+++ MATLAB/functions/ddz_central.m
@@ -3,11 +3,11 @@ df=zeros([size(f)]);
 
 for i=1:size(f,1)
     for j=1:size(f,2)
-        % df(i,j,1)=(-3*f(i,j,1)+4*f(i,j,2)-1*f(i,j,3))/(2*dz); %Boundary condition forward
+        df(i,j,1)=(-3*f(i,j,1)+4*f(i,j,2)-1*f(i,j,3))/(2*dz); %Boundary condition forward
         for k=2:size(f,3)-1
             df(i,j,k)=(f(i,j,k+1)-f(i,j,k-1))/(2*dz); %backward
         end
-        % df(size(f,3))=-1*(-3*f(i,j,size(f,3))+4*f(i,j,size(f,3)-1)-1*f(i,j,size(f,3)-2))/(2*dz); %Boundary condition backwards  
+        df(size(f,3))=-1*(-3*f(i,j,size(f,3))+4*f(i,j,size(f,3)-1)-1*f(i,j,size(f,3)-2))/(2*dz); %Boundary condition backwards  
     end
 end
 end
