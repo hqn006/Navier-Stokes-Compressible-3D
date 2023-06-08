1bc0bd4c6ddb34fe9d6430f1480af03754b9fd7e
hqn006 <hqn006@ucsd.edu>
2023-06-07 22:15:29 -0700

    Revert "Update ddz_fwd.m"
    This reverts commit 2e2a016e0fc3f66d858dadbcf0ac1d0d66d7b805.

--- MATLAB/functions/ddz_fwd.m
+++ MATLAB/functions/ddz_fwd.m
@@ -7,7 +7,7 @@ for i=1:size(f,1)
             df(i,j,k)=(f(i,j,k+1)-f(i,j,k))/dz;
         end
         %Normal
-        % BC df(size(f,3))=(f(i,j,size(f,3)) -f(i,j,size(f,3)-1))/dz;
+        df(size(f,3))=(f(i,j,size(f,3)) -f(i,j,size(f,3)-1))/dz;
     %Periodic
        % df(i,end)=(f(i,1)-f(i,end))/dy;
     end
