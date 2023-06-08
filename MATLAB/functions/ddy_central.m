79dcb3e1dbcbb2813ba1863b421f0d41cd931bc5
hqn006 <hqn006@ucsd.edu>
2023-06-07 22:15:43 -0700

    Revert "Update ddy_central.m"
    This reverts commit 99227c505653f77152ab1b0c4dc9f8ef92da7cff.

--- MATLAB/functions/ddy_central.m
+++ MATLAB/functions/ddy_central.m
@@ -4,11 +4,11 @@ df=zeros([size(f)]);
 
 for i=1:size(f,1)
     for k=1:size(f,3)
-       % df(i,1,k)=((-3*f(i,1,k)+4*f(i,2,k)-1*f(i,3,k))/(2*dy));
+        df(i,1,k)=((-3*f(i,1,k)+4*f(i,2,k)-1*f(i,3,k))/(2*dy));
         for j=2:size(f,2)-1
             df(i,j,k)=(f(i,j+1,k)-f(i,j-1,k))/(2*dy); %backward
         end
-      %  df(size(f,2))=-1*(-3*f(i,size(f,2),k )+4*f(i, size(f,2)-1,k)-1*f(i,size(f,2)-2,k))/(2*dy);
+        df(size(f,2))=-1*(-3*f(i,size(f,2),k )+4*f(i, size(f,2)-1,k)-1*f(i,size(f,2)-2,k))/(2*dy);
     df;
     end
 end
